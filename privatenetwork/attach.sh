bdfcf0a94d8bef0625365017a0adeed6653532e5
李胜 <lisheng@lishengs-MacBook-Pro.local>
2021-04-20 23:09:45 +0800

    testnet

---
+++ privatenetwork/attach.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+/Users/lisheng/Downloads/hscchains/privatenetwork/geth --exec 'admin.nodeInfo.enode' attach data/geth.ipc
+
--- privatenetwork/nodeinfo.sh
+++ privatenetwork/nodeinfo.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-/Users/lisheng/Downloads/hscchains/privatenetwork/geth --exec 'admin.nodeInfo.enode' attach data/geth.ipc
+/Users/lisheng/Downloads/hscchains/privatenetwork/geth  attach data/geth.ipc
 
